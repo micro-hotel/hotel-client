5ad918395f88b42390bb5c8aa5210d29f530054d
inosh611 <inoshharsha531@gmail.com>
2026-03-04 19:49:35 +0530

    resolve conflict

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer-component/footer-component';
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, FooterComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('hotel-client');
+}
