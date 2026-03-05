c3c3c830b0bcc2db334775dd02e1373cfb080122
inosh611 <inoshharsha531@gmail.com>
2026-03-05 09:30:24 +0530

    Setup Angular Material

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-
+import {MainHeader} from "./inner-items/main-header/main-header";
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [MainHeader],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
 export class HomePage {
 
 }
+
